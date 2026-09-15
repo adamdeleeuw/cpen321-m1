8048612b65e1b0083b0c978ca1ca14c984c54bc8
Adam de Leeuw <adamdeleeuw06@gmail.com>
2026-09-14 22:17:31 -0700

    Add API endpoints to app and middleware for json parsing

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,23 @@
 import express, { type Express, type Request, type Response} from 'express';
-import healthRoute from './routes/health.route'
+import healthRoute from './routes/health.route';
+import authRoute from './routes/auth.route';
+import myNameRoute from './routes/my-name.route';
+import connectionInfoRoute from './routes/connection-info.route';
 
-const SUCCESS_CODE = 404;
+const NOT_FOUND = 404;
 
 export function createApp(): Express {
   const app = express();
 
+  app.use(express.json());   // middleware: allows for defined req.body access
+
   app.use('/', healthRoute);
+  app.use('/api/auth', authRoute);
+  app.use('/api/my-name', myNameRoute);
+  app.use('/api/connection-info', connectionInfoRoute);
 
   app.use((_req: Request, res: Response) => {
-    res.status(SUCCESS_CODE).json({ error: 'Not Found' });
+    res.status(NOT_FOUND).json({ error: 'Not Found' });
   });
 
   return app;
